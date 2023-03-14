15248b1ef207cfb9011085cb433761d061acd0c9
adsi_corinto <colede7774@kuvasin.com>
2023-03-14 16:45:48 -0500

    models-cli-add

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class cliente(models.Model):
+    nombre = models.CharField(max_length=30)
+    direccion=models.CharField(max_length=50)
+    email=models.EmailField(max_length=30)
+    telefono=models.CharField(max_length=20)
